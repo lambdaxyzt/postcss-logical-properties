a7aeecdd13dbb6d100ad3407dc29bbcbdb645ba9
lambdaxyzt <32255678+lambdaxyzt@users.noreply.github.com>
2021-01-04 21:59:32 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 'use strict';
 
-var postcss = require('postcss');
+let postcss = require('postcss');
 
-var BASE_DIRECTION  = 'ltr';
-var whitelistProps  = /^(float|clear|text-align)/;
-var logicalValues   = ['inline-start', 'inline-end', 'start', 'end'];
+let BASE_DIRECTION  = 'ltr';
+let whitelistProps  = /^(float|clear|text-align)/;
+let logicalValues   = ['inline-start', 'inline-end', 'start', 'end'];
 
-module.exports = postcss.plugin('postcss-logical-properties', function (opts) {
+module.exports = postcss.plugin('postcss-logical-properties', opts => {
 
-  opts = opts || {};
-
-  return function (css) {
-
-    var options = {};
-    options.rootDir       = (opts.rootDir  !== undefined) ? opts.rootDir  : BASE_DIRECTION;
-    options.replace       = (opts.replace  !== undefined) ? opts.replace  : false;
-    options.html          = (opts.html     !== undefined) ? opts.html     : true;
-
+  opts.rootDir       = (opts.rootDir  !== undefined) ? opts.rootDir  : BASE_DIRECTION;
+  opts.replace       = (opts.replace  !== undefined) ? opts.replace  : false;
+  opts.html          = (opts.html     !== undefined) ? opts.html     : true;
+  
+  // beginning computations from here
+ 
+  return css => {
+    //check root of ("css" or "html") for text-direction
     if (options.html) {
       // First, check root direction
       css.walkRules(function (rule) {
@@ -33,12 +32,10 @@ module.exports = postcss.plugin('postcss-logical-properties', function (opts) {
     // Use the proper value based on the direction
     css.walkRules(function (rule) {
       rule.each(function (decl, i) {
-        if (decl.type !== 'decl') { return; }
+        if (decl.type !== 'decl')  return; 
         var value = decl.value;
         var prop = decl.prop;
-        if(!whitelistProps.test(prop)) {
-          return;
-        }
+        if(!whitelistProps.test(prop)) return;
         if (logicalValues.indexOf(value) !== -1) {
           if (options.rootDir === 'ltr') {
             switch(value) {
